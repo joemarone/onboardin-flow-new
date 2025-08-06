c862b39a6da8fd2fb61cfd786f468557960f60db
joemarone <joe.marone@2hourlearning.com>
2025-08-06 03:54:43 -0400

    Remove all Gate references

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Gate from "./Gate.js"; // import the gate
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Gate>
-    <App />
-  </Gate>
-);
+root.render(<App />);
